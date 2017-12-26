86da3712b6d9603335af1d4dcb8da668f379b319
Max <ipust13@gmail.com>
2017-12-26 14:19:58 +0300

    beauty

--- chi.py
+++ chi.py
@@ -5,11 +5,6 @@ from scipy.sparse import spmatrix
 
 
 def _chisquare(f_obs, f_exp):
-    """Fast replacement for scipy.stats.chisquare.
-
-    Version from https://github.com/scipy/scipy/pull/2525 with additional
-    optimizations.
-    """
     f_obs = np.asarray(f_obs, dtype=np.float64)
 
     k = len(f_obs)
@@ -24,24 +19,6 @@ def _chisquare(f_obs, f_exp):
 
 
 def safe_sparse_dot(a, b, dense_output=False):
-    """Dot product that handle the sparse matrix case correctly
-
-    Uses BLAS GEMM as replacement for numpy.dot where possible
-    to avoid unnecessary copies.
-
-    Parameters
-    ----------
-    a : array or sparse matrix
-    b : array or sparse matrix
-    dense_output : boolean, default False
-        When False, either ``a`` or ``b`` being sparse will yield sparse
-        output. When True, output will always be an array.
-
-    Returns
-    -------
-    dot_product : array or sparse matrix
-        sparse if ``a`` or ``b`` is sparse and ``dense_output=False``.
-    """
     if isinstance(a, spmatrix) or isinstance(b, spmatrix):
         ret = a * b
         if dense_output and hasattr(ret, "toarray"):
@@ -52,52 +29,11 @@ def safe_sparse_dot(a, b, dense_output=False):
 
 
 def chi2(X, y):
-    """Compute chi-squared stats between each non-negative feature and class.
-
-    This score can be used to select the n_features features with the
-    highest values for the test chi-squared statistic from X, which must
-    contain only non-negative features such as booleans or frequencies
-    (e.g., term counts in document classification), relative to the classes.
-
-    Recall that the chi-square test measures dependence between stochastic
-    variables, so using this function "weeds out" the features that are the
-    most likely to be independent of class and therefore irrelevant for
-    classification.
-
-    Read more in the :ref:`User Guide <univariate_feature_selection>`.
-
-    Parameters
-    ----------
-    X : {array-like, sparse matrix}, shape = (n_samples, n_features_in)
-        Sample vectors.
-
-    y : array-like, shape = (n_samples,)
-        Target vector (class labels).
-
-    Returns
-    -------
-    chi2 : array, shape = (n_features,)
-        chi2 statistics of each feature.
-    pval : array, shape = (n_features,)
-        p-values of each feature.
-
-    Notes
-    -----
-    Complexity of this algorithm is O(n_classes * n_features).
-
-    See also
-    --------
-    f_classif: ANOVA F-value between label/feature for classification tasks.
-    f_regression: F-value between label/feature for regression tasks.
-    """
-
-    # XXX: we might want to do some of the following in logspace instead for
-    # numerical stability.
     Y = np.vstack((y + 1) / 2)
     if Y.shape[1] == 1:
         Y = np.append(1 - Y, Y, axis=1)
 
-    observed = safe_sparse_dot(Y.T, X)  # n_classes * n_features
+    observed = safe_sparse_dot(Y.T, X)
 
     feature_count = X.sum(axis=0).reshape(1, -1)
     class_prob = Y.mean(axis=0).reshape(1, -1)
